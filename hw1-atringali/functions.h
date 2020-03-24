33d7434b37fa1b6215ab8c5e61c84f1c36b861bd
AdamTringali <aatringali@gmail.com>
2020-03-24 13:06:23 -0400

    update2

--- hw1-atringali/functions.h
+++
@@ -1,6 +0,0 @@
-int readWriteFile(int fd1, void* buf , int len, int fd2, long debugs, 
-    unsigned char *key,  unsigned char *iv, unsigned char *ciphertext, int crypt);
-int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
-            unsigned char *iv, unsigned char *ciphertext, long debugs);
-int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
-            unsigned char *iv, unsigned char *plaintext, long debugs);
